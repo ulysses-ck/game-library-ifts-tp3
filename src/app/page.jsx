801a5849f075142ebb87b0637906e7dcd9afc6b0
ulysses-ck <ulisesapaza.280@gmail.com>
2024-05-29 20:58:41 -0300

    fix: interchange api route with csv for supabase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,35 +1,33 @@
-export default async function Home() {
-  const data = await fetch("http://localhost:3000/api/all");
-  const games = await data.json();
+import { notFound } from "next/navigation";
+import { supabase } from "../supabase";
 
+export default async function Home() {
+  const { data, error } = await supabase
+    .from("games")
+    .select("Rank, Platform, Name")
+    .limit(10);
+
+  if (error) {
+    return notFound();
+  }
   return (
     <main>
-      <h1>Game Library</h1>
-
-      <ul className="flex flex-col">
-        {games.map((game) => (
-          <li className="grid grid-cols-7 " key={game.Rank}>
-            <h2>{game.Name}</h2>
-            <p>
-              <strong>Rank:</strong> {game.Rank}
-            </p>
-            <p>
-              <strong>Platform:</strong> {game.Platform}
-            </p>
-            <p>
-              <strong>Year:</strong> {game.Year}
-            </p>
-            <p>
-              <strong>Genre:</strong> {game.Genre}
-            </p>
-            <p>
-              <strong>Publisher:</strong> {game.Publisher}
-            </p>
-            <p>
-              <strong>Global Sales:</strong> {game.Global_Sales}
-            </p>
-          </li>
-        ))}
+      <h1>Proyecto Juegos</h1>
+      <ul className="[&>*:nth-child(odd)]:bg-[#d5e6ea] [&>*:nth-child(even)]:bg-[#e9d3ea]">
+        <li className="grid grid-cols-3 items-center bg-gradient-to-r from-[#F053E0] from-0% to-[#0165FB] to-100%">
+          <p className="text-center">ID</p>
+          <p className="text-center">Nombre</p>
+          <p className="text-center">Plataforma</p>
+        </li>
+        {data.map((game) => {
+          return (
+            <li key={game.Rank} className="grid grid-cols-3 items-center">
+              <p className="text-center">{game.Rank}</p>
+              <p className="text-center">{game.Name}</p>
+              <p className="text-center">{game.Platform}</p>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
