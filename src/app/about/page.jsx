747292270063d556b48a1e4bb2b81207062e857b
ulysses-ck <ulisesapaza.280@gmail.com>
2024-05-29 21:36:39 -0300

    feat: create sample pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function page() {
+  return (
+    <div>
+      <h1>About Page</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cum
+        necessitatibus sequi consequuntur molestias tempora, corporis veritatis
+        doloremque, repudiandae vero corrupti exercitationem iure facere
+        adipisci nesciunt id dicta excepturi voluptatum.
+      </p>
+    </div>
+  );
+}
