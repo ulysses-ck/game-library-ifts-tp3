b4fe515ecb7481def3c0f58b35278816a7647b98
ulysses-ck <ulisesapaza.280@gmail.com>
2024-05-29 21:08:45 -0300

    style: position main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default async function Home() {
     return notFound();
   }
   return (
-    <main>
+    <main className={styles.mainSection}>
       <h1>Proyecto Juegos</h1>
       <ul className={styles.tableUl}>
         <li className={styles.tableHeaders}>
