88d98afc78225f7e4668c5d05a01d99affab3e91
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-09 04:37:16 -0300

    refactor: Update SearchPage title styling to use CSS class

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function SearchPage() {
   return (
     <main className={styles.SearchPage}>
-      <h1>Search Game by Name</h1>
+      <h1 className="text-2xl">Search Game by Name</h1>
 
       <SearchBar />
     </main>
