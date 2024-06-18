d80d0ef4ed194fe8d511e22e9849dc1a061df43f
enzojb <enzobarosi@gmail.com>
2024-06-18 16:56:20 -0300

    Fix: Modal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,47 +1,28 @@
 "use client";
 import { useFormState } from "react-dom";
+
 import styles from "./page.module.css";
 import Link from "next/link";
+
 import { deleteGame, getGames } from "./actions";
 import { useEffect, useState } from "react";
-import ConfirmationModal from "../components/Modal"; // Ajusta la ruta según donde esté definido tu componente ConfirmationModal
+
+import ConfirmationModal from "../components/Modal/index";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
-  const [gameIdToDelete, setGameIdToDelete] = useState(null);
-  const [state, formAction] = useFormState(getGames, { data: [] }); // Asegúrate de inicializar state.data como un arreglo vacío
+  const [state, formAction] = useFormState(getGames, { data: [] });
   const [limit, setLimit] = useState(10);
-  const [isLoading, setIsLoading] = useState(false); // Estado para manejar la carga de datos
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [gameToDelete, setGameToDelete] = useState(null); //
 
-  const openDeleteModal = (gameId) => {
-    setGameIdToDelete(gameId);
-    setShowModal(true);
+  const handleDeleteModalOpen = (gameId) => {
+    setGameToDelete(gameId);
+    setDeleteModalOpen(true);
   };
 
-  const closeModal = () => {
-    setShowModal(false);
-    setGameIdToDelete(null);
-  };
-
-  const handleConfirmDelete = () => {
-    if (gameIdToDelete) {
-      const formData = new FormData();
-      formData.append("Rank", gameIdToDelete);
-      setIsLoading(true); // Indicar que se está realizando una operación de carga
-      deleteGame(formData)
-        .then(() => {
-          console.log(`Juego con ID ${gameIdToDelete} eliminado`);
-          formAction({ limit: limit }); // Actualizar los datos después de la eliminación
-        })
-        .catch((error) => {
-          console.error("Error al eliminar el juego:", error);
-        })
-        .finally(() => {
-          setIsLoading(false); // Finalizar la indicación de carga
-          setShowModal(false); // Cerrar el modal después de la eliminación
-          setGameIdToDelete(null);
-        });
-    }
+  const handleDeleteModalClose = () => {
+    setDeleteModalOpen(false);
+    setGameToDelete(null);
   };
 
   const handleOnInput = (e) => {
@@ -74,51 +55,69 @@ export default function Home() {
         </label>
       </div>
 
-      {isLoading ? (
-        <p>Cargando juegos...</p>
-      ) : (
-        <div className={styles.tableContainer}>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Plataforma</th>
-                <th>Año</th>
-                <th>Género</th>
-                <th>Desarrollador</th>
-                <th>Ventas</th>
-                <th>Acción</th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.data.map((game) => (
-                <tr key={game.Rank}>
-                  <td>{game.Rank}</td>
-                  <td>{game.Name}</td>
-                  <td>{game.Platform}</td>
-                  <td>{game.Year}</td>
-                  <td>{game.Genre}</td>
-                  <td>{game.Publisher}</td>
-                  <td>{game.Sales}</td>
-                  <td>
-                    <div className={styles.ActionButtons}>
-                      <Link className={styles.editButton} href={`/edit/${game.Rank}`}>
-                        ✏️
-                      </Link>
-                      <button onClick={() => openDeleteModal(game.Rank)} className={styles.deleteButton}>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Plataforma</th>
+              <th>Año</th>
+              <th>Género</th>
+              <th>Desarrollador</th>
+              <th>Ventas</th>
+              <th>Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.data.map((game) => (
+              <tr key={game.Rank}>
+                <td>{game.Rank}</td>
+                <td>{game.Name}</td>
+                <td>{game.Platform}</td>
+                <td>{game.Year}</td>
+                <td>{game.Genre}</td>
+                <td>{game.Publisher}</td>
+                <td>{game.Sales}</td>
+                <td>
+                  <div className={styles.ActionButtons}>
+                    <Link
+                      className={styles.editButton}
+                      href={`/edit/${game.Rank}`}
+                    >
+                      ✏️
+                    </Link>
+                    <form action={deleteGame}>
+                      <input type="hidden" name="Rank" value={game.Rank} />
+                      <button
+                        className={styles.deleteButton}
+                        onClick={() => handleDeleteModalOpen(game.Rank)}
+                      >
                         🗑️
                       </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-
-      <ConfirmationModal isOpen={showModal} onCancel={closeModal} onConfirm={handleConfirmDelete} />
+                    </form>
+                    <ConfirmationModal
+                      isOpen={deleteModalOpen}
+                      onCancel={handleDeleteModalClose}
+                      onConfirm={() => {
+                        const formData = new FormData();
+                        formData.append("Rank", gameToDelete);
+                        deleteGame(formData).then(() => {
+                          const formData = new FormData();
+                          formData.append("limit", limit);
+                          formAction(formData);
+                        });
+                        handleDeleteModalClose();
+                      }}
+                      gameId={gameToDelete}
+                    />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -49,8 +49,7 @@ const ConfirmationModal = ({ isOpen, onCancel, onConfirm, gameId }) => {
             </button>
             <button
               className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              onClick={handleConfirm}
+              type="button" onClick={handleConfirm}
             >
               Eliminar
             </button>
