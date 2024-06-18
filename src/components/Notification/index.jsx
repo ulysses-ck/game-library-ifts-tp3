3b115fe7b379818cb05ef584b2c1fbf59cd52772
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-18 01:53:37 -0300

    refactor: Update Notification component to use dynamic action and success status

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -1,18 +1,25 @@
+"use client";
 import styles from "./Notification.module.css";
 import closeSVG from "../../../public/close-icon.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Notification({ action, isSuccess }) {
+  const [show, setShow] = useState(isSuccess);
+
+  const handleOnClick = () => {
+    setShow(false);
+  };
+
   return (
     <div
       className={isSuccess ? styles.notificacionGreen : styles.notificacionRed}
+      style={{ display: show ? "flex" : "none" }}
     >
       <p className={styles.textNotification}>
-        {isSuccess
-          ? `Éxito al ${action} Juego`
-          : `Error al ${action} Juego`}
+        {isSuccess ? `Éxito al ${action} Juego` : `Error al ${action} Juego`}
       </p>
-      <button className={styles.btnNotification}>
+      <button className={styles.btnNotification} onClick={handleOnClick}>
         <Image className={styles.imgNotification} src={closeSVG} alt="Close" />
       </button>
     </div>
