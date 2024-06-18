96ea1454b686b16b6a7a1c663c332563d55d6147
NicoRepa <nicolasreparaz@gmail.com>
2024-06-17 21:56:02 -0300

    add: notifications

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -1,3 +1,44 @@
+import styles from "./Notification.module.css";
+
+
 export default function Notification() {
-  return <div>Notification</div>;
+  return(
+    <>
+    <div className={styles.notificacionGreen}>
+      <p className={styles.textNotification}>Juego agregado exitosamente</p>
+      <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+
+    <div className={styles.notificacionGreen}>
+        <p className={styles.textNotification}>Juego modificado exitosamente</p>
+        <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+
+    <div className={styles.notificacionGreen}>
+        <p className={styles.textNotification}>Juego eliminado exitosamente</p>
+        <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+
+    <div className={styles.notificacionRed}>
+      <p className={styles.textNotification}>Error al agregar un juego</p>
+      <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+
+    <div className={styles.notificacionRed}>
+        <p className={styles.textNotification}>Error al modificar un juego</p>
+        <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+
+    <div className={styles.notificacionRed}>
+        <p className={styles.textNotification}>Error al eliminar un juego</p>
+        <button className={styles.btnNotification}><img className={styles.imgNotification} src="../../../public/close-icon.svg" /></button>
+    </div>
+    
+    
+    </>
+  
+  
+  
+  
+  );
 }
