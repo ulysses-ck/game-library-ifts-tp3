dcb4f83cbfd6ab84ec0b0800e3bd08a9752dc1f4
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-19 16:55:54 -0300

    feat: decouple logic into a new component formentries

--- src/components/FormEntries/index.jsx
+++ src/components/FormEntries/index.jsx
@@ -1,3 +1,35 @@
-export default function FormEntries() {
-  return <div>FormEntries</div>;
+"use client";
+import { useEffect, useState } from "react";
+
+export default function FormEntries({formAction}) {
+  const [limit, setLimit] = useState(10);
+
+  const handleOnInput = (e) => {
+    const limit = e.target.value;
+    setLimit(limit);
+  };
+
+  useEffect(() => {
+    if (limit) {
+      const formData = new FormData();
+      formData.append("limit", limit);
+      formAction(formData);
+    }
+  }, [limit]);
+
+  return (
+    <div>
+      <label htmlFor="limit">
+        Mostrar
+        <input
+          type="number"
+          id="limit"
+          onInput={handleOnInput}
+          value={limit}
+          className="border border-black"
+        />
+        entradas
+      </label>
+    </div>
+  );
 }
