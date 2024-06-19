94dbdfa74b0ebbe69a53b52b8727f32e98510905
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-19 17:37:36 -0300

    feat: Add logic to pagination buttons component

--- src/components/PaginationButtons/index.jsx
+++ src/components/PaginationButtons/index.jsx
@@ -1,12 +1,42 @@
+"use client";
 import styles from "./PaginationButtons.module.css";
 
-export default function PaginationButtons() {
+import { useState } from "react";
+
+export default function PaginationButtons({ formAction, limit, offset }) {
+  const [page, setPage] = useState(1);
+
+  const handleOnClickNext = () => {
+    setPage((prevPage) => prevPage + 1);
+
+    const formData = new FormData();
+    formData.append("limit", limit);
+    formData.append("offset", limit * page + 1);
+    formAction(formData);
+  };
+
+  const handleOnClickPrevious = () => {
+    setPage((prevPage) => (prevPage < 1 ? 1 : prevPage - 1));
+    const formData = new FormData();
+    formData.append("limit", limit);
+    formData.append("offset", offset - limit < 0 ? 0 : offset - limit);
+    formAction(formData);
+  };
+
   return (
     <div className={styles.pagination}>
       <div className={styles.container}>
-        <button className={styles.btn}>Anterior</button>
-        <span className={styles.btn}>1</span>
-        <button className={styles.btn}>Siguiente</button>
+        <button
+          className={styles.btn}
+          disabled={page === 1}
+          onClick={handleOnClickPrevious}
+        >
+          Anterior
+        </button>
+        <span className={styles.btn}>{page}</span>
+        <button className={styles.btn} onClick={handleOnClickNext}>
+          Siguiente
+        </button>
       </div>
     </div>
   );
