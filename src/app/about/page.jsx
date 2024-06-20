67cd941c186802ecf4ad53c5f6a1747b36244409
Analia Fernandez <analiaguadalupefernandez@gmail.com>
2024-06-20 19:43:25 -0300

    AboutUs & Contact
    Se agregó la parte de AboutUs y de Contact

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,51 @@
+import { BLOCKED_PAGES } from "next/dist/shared/lib/constants";
+import styles from "./about.module.css";
+
 export default function page() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className={styles.completeText}>
+      <h1 className={styles.mainTitle}>Sobre nosotros</h1>
+        <p className={styles.paragraphs}>
+        Somos un grupo de estudiantes apasionados por los videojuegos y la tecnología, 
+        que se conocieron cursando en un terciario de desarrollo de software. Nuestra 
+        iniciativa surgió del deseo de crear un espacio donde cualquier persona, sin importar
+        su nivel de conocimiento, pudiera acceder a una amplia base de datos de videojuegos.
+          Así nació Proyecto Videojuegos.
+        </p>
+      
+      <h2 className={styles.secondaryTitle}>Nuestro propósito</h2>
+        <p className={styles.paragraphs}>
+        Queremos que la información sobre videojuegos esté disponible para todos, ya sean fanáticos 
+        empedernidos, coleccionistas, desarrolladores en busca de inspiración, o simplemente curiosos sobre la historia y evolución de los 
+        videojuegos. Creemos que el acceso libre a la información fomenta la creatividad, 
+        la innovación y una mayor apreciación de este arte interactivo.
+        </p>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cum
-        necessitatibus sequi consequuntur molestias tempora, corporis veritatis
-        doloremque, repudiandae vero corrupti exercitationem iure facere
-        adipisci nesciunt id dicta excepturi voluptatum.
-      </p>
+      <h2 className={styles.secondaryTitle}>Nuestro Proyecto</h2>
+        <div className={styles.paragraphs}>
+        Este proyecto es el resultado de meses de trabajo colaborativo. Utilizamos tecnologías 
+        modernas para desarrollar una web que te permita:
+        </div>
+        <div className={styles.paragraphs}>
+          <strong>Buscar y explorar:</strong> una base de datos con videojuegos de todas las épocas y 
+          plataformas, donde podes consultar información detallada.
+        </div>
+        <div className={styles.paragraphs}>
+          <strong>Crear y compartir:</strong> podes añadir nuevos videojuegos, contribuyendo a nuestra base de datos, 
+          compartiendo tus conocimientos y descubrimientos con la comunidad.
+        </div>
+        <div className={styles.paragraphs}>
+          <strong>Actualizar y mejorar:</strong> para mantener la información existente precisa y relevante, podes 
+          editarla y actualizarla .
+        </div>
+        <div className={styles.paragraphs}>
+          <strong>Eliminar contenido erróneo:</strong> con tu ayuda podremos mantener la calidad de la base de datos eliminando 
+          información inexacta o duplicada.
+        </div>
+        <div className={styles.specialParagraph}>
+        Gracias por ser parte de esta aventura con nosotros. ¡Esperamos que disfrutes explorando 
+        nuestro sitio tanto como nosotros disfrutamos creándolo! 
+        </div>
     </div>
   );
 }
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,90 @@
+import styles from "./contact.module.css";
+
+
 export default function page() {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className={styles.completeText}>
+      <h1 className={styles.mainTitle}>¡Contactanos y hagamos un proyecto juntos!</h1>
+      <div className={styles.paragraphs}>
+        <p>
+          Gracias por visitar Proyecto Videojuegos, nos encanta que estés acá. 
+          Si tenes alguna propuesta y deseas nuestra colaboración, no dudes en 
+          contactarnos en nuestras redes sociales, nos encantaría poder ayudarte a concretar tu idea. 
+          También podes pasar por nuestros portfolios para conocer más a cerca 
+          de nuestros proyectos individuales y las herramientas que manejamos. 
+          ¡Esperamos saber pronto de vos!
+        </p>
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cum
-        necessitatibus sequi consequuntur molestias tempora, corporis veritatis
-        doloremque, repudiandae vero corrupti exercitationem iure facere
-        adipisci nesciunt id dicta excepturi voluptatum.
-      </p>
+      <div className={styles.mainContainer}>
+        <div className={styles.secondaryContainer}>
+          <img src="ulises.png" className= {styles.imageShape}/>
+          <div className={styles.nameStyle}>Ulises Apaza</div>
+          <div className={styles.nameStyle}>Full Stack Developer</div>
+          <div class={styles.socialFollow}>
+            <div class={styles.thirdContainer}>
+                <a href="https://www.linkedin.com/in/ulisesapaza/">
+                    <img src="linkedin.png" alt="Logo Linkedin" className={styles.logoSize}/>
+                </a>
+                <a href="https://github.com/ulysses-ck">
+                    <img src="github.png" alt="Logo Github" className={styles.logoSize}/>
+                </a>
+            </div>
+          </div>        
+        </div>
+
+        <div className={styles.secondaryContainer}>
+          <img src="enzo.png" className= {styles.imageShape}/>
+          <div className={styles.nameStyle}> Enzo Barosi</div>
+          <div className={styles.nameStyle}>Full Stack Developer</div>
+          <div class={styles.socialFollow}>
+            <div class={styles.thirdContainer}>
+              <a href="https://www.linkedin.com/in/enzo-barosi/">
+                    <img src="linkedin.png" alt="Logo Linkedin" className={styles.logoSize}/>
+              </a>
+              <a href="https://github.com/enzojb">
+                  <img src="github.png" alt="Logo Github" className={styles.logoSize}/>
+              </a>
+            </div>
+          </div>
+        </div>
+        
+        <div className={styles.secondaryContainer}>
+          <img src="nicolas.png" className= {styles.imageShape}/>
+          <div className={styles.nameStyle}>Nicolas Reparas</div>
+          <div className={styles.nameStyle}>Full Stack Developer</div>
+          <div>
+          <div class={styles.socialFollow}>
+            <div class={styles.thirdContainer}>
+            <a href="https://www.linkedin.com/in/nicolas-perez-reparaz">
+                    <img src="linkedin.png" alt="Logo Linkedin" className={styles.logoSize}/>
+                </a>
+                <a href="https://github.com/NicoRepa">
+                    <img src="github.png" alt="Logo Github" className={styles.logoSize}/>
+                </a>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <div className={styles.secondaryContainer}>
+          <img src="anitaAvatar.jpg" className= {styles.imageShape}/>
+          <div className={styles.nameStyle}>Analia Fernández</div>
+          <div className={styles.nameStyle}>Frontend Developer</div>
+          <div>
+          <div class={styles.socialFollow}>
+            <div class={styles.thirdContainer}>
+            <a href="https://www.linkedin.com/in/analia-guadalupe-fernández">
+                    <img src="linkedin.png" alt="Logo Linkedin" className={styles.logoSize}/>
+                </a>
+                <a href="https://github.com/AnaliaGF">
+                    <img src="github.png" alt="Logo Github" className={styles.logoSize}/>
+                </a>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
