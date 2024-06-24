eb92916a9b14b38ed46ed8c1cc5947098ca35dc2
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-24 18:41:09 -0300

    feat: Refactor Home component to use TableRecords for displaying game data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,6 +9,7 @@ import { getGames } from "./actions";
 import ButtonDelete from "@/components/ButtonDelete";
 import PaginationButtons from "@/components/PaginationButtons";
 import FormEntries from "@/components/FormEntries";
+import TableRecords from "@/components/TableRecords";
 
 export default function Home() {
   const [state, formAction] = useFormState(getGames, {
@@ -22,52 +23,13 @@ export default function Home() {
     <main className={styles.mainSection}>
       <FormEntries formAction={formAction} />
       <div className={styles.tableContainer}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Plataforma</th>
-              <th>Año</th>
-              <th>Género</th>
-              <th>Desarrollador</th>
-              <th>Ventas</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {state.data.map((game) => (
-              <tr key={game.Rank}>
-                <td>{game.Rank}</td>
-                <td>{game.Name}</td>
-                <td>{game.Platform}</td>
-                <td>{game.Year}</td>
-                <td>{game.Genre}</td>
-                <td>{game.Publisher}</td>
-                <td>
-                {parseFloat(game.NA_Sales) +
-                  parseFloat(game.EU_Sales) +
-                  parseFloat(game.JP_Sales) +
-                  parseFloat(game.Other_Sales)}{" "}
-                M
-              </td>
-                <td>
-                  <div className={styles.ActionButtons}>
-                    <Link
-                      className={styles.editButton}
-                      href={`/edit/${game.Rank}`}
-                    >
-                      ✏️
-                    </Link>
-                    <ButtonDelete game={game} />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <TableRecords data={state.data} />
       </div>
-      <PaginationButtons limit={state.limit} offset={state.offset} formAction={formAction} />
+      <PaginationButtons
+        limit={state.limit}
+        offset={state.offset}
+        formAction={formAction}
+      />
     </main>
   );
 }
