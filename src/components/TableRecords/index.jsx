1996bbef58a312d905e82023ac7046b6ccbc5656
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-24 18:56:05 -0300

    feat: add conditional rendering for table records

--- src/components/TableRecords/index.jsx
+++ src/components/TableRecords/index.jsx
@@ -31,19 +31,22 @@ export default function TableRecords({ data }) {
             <td>{game.Year}</td>
             <td>{game.Genre}</td>
             <td>{game.Publisher}</td>
-            <td>{game.NA_Sales}</td>
-            <td>{game.EU_Sales}</td>
-            <td>{game.JP_Sales}</td>
-            <td>{game.Other_Sales}</td>
-            <td>{game.Global_Sales}</td>
+            <td>{game.NA_Sales ? `${game.NA_Sales} M` : "No especificado"}</td>
+            <td>{game.EU_Sales ? `${game.EU_SalesE} M` : "No especificado"}</td>
+            <td>{game.JP_Sales ? `${game.JP_Sales} M` : "No especificado"}</td>
             <td>
-                <div className={styles.ActionButtons}>
-                    <Link className={styles.editButton} href={`/edit/${game.Rank}`}>
-                    ✏️
+              {game.Other_Sales ? `${game.Other_Sales} M` : "No especificado"}
+            </td>
+            <td>
+              {game.Global_Sales ? `${game.Global_Sales} M` : "No especificado"}
+            </td>
+            <td>
+              <div className={styles.ActionButtons}>
+                <Link className={styles.editButton} href={`/edit/${game.Rank}`}>
+                  ✏️
                 </Link>
                 <ButtonDelete game={game} />
-                </div>
-
+              </div>
             </td>
           </tr>
         ))}
